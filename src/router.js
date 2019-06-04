e6fc744ef7acc28eb83a575fdf0c8537130999f8
JackMarx <seriousfools@gmail.com>
2019-06-04 09:41:59 -0500

    new recipes

--- src/router.js
+++ src/router.js
@@ -1,23 +1,19 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+import About from './views/About.vue';
+import Test from './views/Test.vue';
+import RecipesNew from './views/RecipesNew.vue';
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
+  hash: false,
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+    {path: '/', name: 'home', component: Home },
+    {path: '/about', name: 'about', component: About },
+    {path: '/test', name: 'test', component: Test },
+    {path: '/recipes/new', name: 'recipes-new', component: RecipesNew }
   ]
 })
